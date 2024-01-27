c465b169c9607f7006aa4f8972515f857e66a7b1
aaronjnc <aaronkingnc@gmail.com>
2024-01-27 01:16:28 -0500

    Started implementing buttons

---
+++ Source/Space_Shifter/Interactables/ButtonComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "ButtonComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACE_SHIFTER_API UButtonComponent : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+};
